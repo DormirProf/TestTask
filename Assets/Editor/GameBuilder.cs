a3bd81052f6c0a6a7159733a96820a98268277de
DormirProf <zheny.garshedsky1@gmail.com>
2024-12-26 19:15:26 +0300

    upload build

--- Assets/Editor/GameBuilder.cs
+++ Assets/Editor/GameBuilder.cs
@@ -28,5 +28,33 @@ namespace Editor
                 Debug.Log("Build failed");
             }
         }
+        
+        [MenuItem("Build/Build Android")]
+        public static void PerformAndroidBuild()
+        {
+            PlayerSettings.Android.keystoreName = "user.keystore";
+            PlayerSettings.Android.keystorePass = "123123";
+            PlayerSettings.Android.keyaliasName = "123";
+            PlayerSettings.Android.keyaliasPass = "123123";
+
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+            buildPlayerOptions.locationPathName = "build/android/TestTask.apk";
+            buildPlayerOptions.target = BuildTarget.Android;
+            buildPlayerOptions.options = BuildOptions.None;
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log("Build failed");
+            }
+        }
     }
 }
